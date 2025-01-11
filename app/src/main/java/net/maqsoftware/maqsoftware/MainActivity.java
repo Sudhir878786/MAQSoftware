f3137fe8f842a9698bb99661b3d65ce59fc4a1e9
Sudhir878786 <sudhirsharma@iitbhilai.ac.in>
2025-01-12 00:44:16 +0530

    added biometric authentication

--- app/src/main/java/net/maqsoftware/maqsoftware/MainActivity.java
+++ app/src/main/java/net/maqsoftware/maqsoftware/MainActivity.java
@@ -1,6 +1,9 @@
 package net.maqsoftware.maqsoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,6 +11,9 @@ import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,38 +24,96 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize WebView
         CustomWebViewClient client = new CustomWebViewClient(this);
 
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(client);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
+        // Check and Prompt for Biometric Authentication
+        authenticateUser();
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()){
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()) {
             this.webView.goBack();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    private void authenticateUser() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()
+        ) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                showBiometricPrompt();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "No biometric hardware available", Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Biometric hardware unavailable", Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "No biometric data enrolled", Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
+                break;
+        }
+    }
+
+    private void showBiometricPrompt() {
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt(this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(MainActivity.this, "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(MainActivity.this, "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setSubtitle("Authenticate to access the WebView")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
+    }
 }
 
-class CustomWebViewClient extends WebViewClient{
+class CustomWebViewClient extends WebViewClient {
     private Activity activity;
-    public CustomWebViewClient(Activity activity){
+
+    public CustomWebViewClient(Activity activity) {
         this.activity = activity;
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView webView, String url){
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
         return false;
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request){
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request) {
         return false;
     }
 }
