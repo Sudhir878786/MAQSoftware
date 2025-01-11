be151b506c50a5f3bce9f510336b7152bb855d79
Sudhir878786 <sudhirsharma@iitbhilai.ac.in>
2025-01-11 14:21:48 +0530

    update build

---
+++ app/build/generated/source/buildConfig/androidTest/debug/net/maqsoftware/maqsoftware/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.maqsoftware.maqsoftware.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.maqsoftware.maqsoftware.test";
+  public static final String BUILD_TYPE = "debug";
+}
