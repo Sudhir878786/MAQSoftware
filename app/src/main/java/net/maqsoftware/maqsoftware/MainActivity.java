f0855cd9f8931e8005835549b79ea472a791b1b4
Sudhir878786 <sudhirsharma@iitbhilai.ac.in>
2025-01-13 21:35:37 +0530

    added loading element

--- app/src/main/java/net/maqsoftware/maqsoftware/MainActivity.java
+++ app/src/main/java/net/maqsoftware/maqsoftware/MainActivity.java
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
-
+import android.widget.RelativeLayout;
+import android.view.View;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -13,25 +14,27 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-import java.util.concurrent.Executor;
 
+import java.util.concurrent.Executor;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private RelativeLayout loadingOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize WebView
-        CustomWebViewClient client = new CustomWebViewClient(this);
-
+        // Initialize WebView and Loading Overlay
         webView = findViewById(R.id.webView);
+        loadingOverlay = findViewById(R.id.loadingOverlay);
+
+        CustomWebViewClient client = new CustomWebViewClient(this, loadingOverlay);
         webView.setWebViewClient(client);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        // Check and Prompt for Biometric Authentication
+        // Authenticate the user before loading the webpage
         authenticateUser();
     }
 
@@ -46,21 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void authenticateUser() {
         BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate()
-        ) {
+        switch (biometricManager.canAuthenticate()) {
             case BiometricManager.BIOMETRIC_SUCCESS:
                 showBiometricPrompt();
                 break;
             case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                Toast.makeText(this, "No biometric hardware available", Toast.LENGTH_SHORT).show();
-                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
-                break;
             case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                Toast.makeText(this, "Biometric hardware unavailable", Toast.LENGTH_SHORT).show();
-                webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
-                break;
             case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                Toast.makeText(this, "No biometric data enrolled", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Biometric not available or not set up", Toast.LENGTH_SHORT).show();
                 webView.loadUrl("https://testmaq.sharepoint.com/myspace/Pages/MySpace.aspx#");
                 break;
         }
@@ -101,19 +97,31 @@ public class MainActivity extends AppCompatActivity {
 }
 
 class CustomWebViewClient extends WebViewClient {
-    private Activity activity;
+    private RelativeLayout loadingOverlay;
 
-    public CustomWebViewClient(Activity activity) {
-        this.activity = activity;
+    public CustomWebViewClient(Activity activity, RelativeLayout loadingOverlay) {
+        this.loadingOverlay = loadingOverlay;
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+    public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+        loadingOverlay.setVisibility(View.VISIBLE); // Show the loader
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        loadingOverlay.setVisibility(View.GONE); // Hide the loader
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         return false;
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request) {
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
         return false;
     }
 }
